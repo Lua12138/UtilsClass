cda5e18c3e9b9da2cfc87e3cddd48cbe70ba49ef
forDream <gam2046@users.noreply.github.com>
2016-10-29 19:12:49 +0800

    other intresting.

--- JavaScriptIntresting.java
+++ JavaScriptIntresting.java
@@ -2,6 +2,8 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
+// 最低需要Java 8支持
+// 在JDK目录下新增一个jjs命令，输入为javascript字符串，效果等同于下面的代码
 public class JavaScriptIntresting {
     private static final String script =
             "function add(a, b){" +
