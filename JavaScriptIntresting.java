00e0c60ab42b0f0e3b9f8edd0c4e79af04e2906a
forDream <gam2046@users.noreply.github.com>
2016-11-01 10:46:09 +0800

    typo

--- JavaScriptIntresting.java
+++ JavaScriptIntresting.java
@@ -2,8 +2,8 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
-// 最低需要Java 8支持
-// 在JDK目录下新增一个jjs命令，输入为javascript字符串，效果等同于下面的代码
+// 最低需要Java 6支持，Java 8中对改变的引擎，速度有显著的提升
+// 在JDK目录下新增一个jjs命令，输入为javascript字符串，效果等同于下面的代码（该命令Java8下存在，6/7未测试）
 public class JavaScriptIntresting {
     private static final String script =
             "function add(a, b){" +
