f33872bb907aa771fc72e544941e913209cd126a
forDream <gam2046@users.noreply.github.com>
2017-02-21 17:10:18 +0800

    [DateTimeUtils] some date-time utils of javascript

---
+++ javascript/DateTimeUtils.js
@@ -0,0 +1,33 @@
+/**
+  * Example :  dateDiff("2016-01-01","2017-01,01","day")
+  * Copy from Internet. Written by unknown
+  */
+function dateDiff(startTime, endTime, diffType) {
+    //将xxxx-xx-xx的时间格式，转换为 xxxx/xx/xx的格式 
+    startTime = startTime.replace(/\-/g, "/");
+    endTime = endTime.replace(/\-/g, "/");
+
+    //将计算间隔类性字符转换为小写
+    diffType = diffType.toLowerCase();
+    var sTime = new Date(startTime); //开始时间
+    var eTime = new Date(endTime); //结束时间
+    //作为除数的数字
+    var divNum = 1;
+    switch (diffType) {
+        case "second":
+            divNum = 1000;
+            break;
+        case "minute":
+            divNum = 1000 * 60;
+            break;
+        case "hour":
+            divNum = 1000 * 3600;
+            break;
+        case "day":
+            divNum = 1000 * 3600 * 24;
+            break;
+        default:
+            break;
+    }
+    return parseInt((eTime.getTime() - sTime.getTime()) / parseInt(divNum));
+}
