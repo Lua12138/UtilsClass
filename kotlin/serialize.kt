a83a1487df92ba95e0fea5bd05dd2fa778a57276
forDream <gam2046@users.noreply.github.com>
2018-02-01 23:30:03 +0800

    [serialize.kt] something of serialize about json

---
+++ kotlin/serialize.kt
@@ -0,0 +1,32 @@
+/**
+ * Serialize an object to a string
+ *
+ * **Depend on Gson**
+ */
+fun Any.toJsonString(): String =
+        Gson().toJson(this)
+
+/**
+ * Convert a string into an object
+ *
+ * **Depend on Gson**
+ *
+ * ## How to Deserialize a generic object (only for **Gson**)
+ *
+ * Suppose you want to deserialize `List<String>`
+ *
+ * Kotlin Code :
+ *
+ * `"Your Json String".toJsonObject(object : TypeToken<List<String>>() {}.type)`
+ *
+ * <del>
+ * Java Code :
+ *
+ * `new TypeToken<List<String>>() {}.getType();`
+ * </del>
+ *
+ * @param targetClass Target type of conversion
+ * @return Target type of conversion
+ */
+fun <T> String.toJsonObject(targetClass: Class<out T>): T =
+        Gson().fromJson<T>(this, targetClass)
